12cf183bb7f952c1de5470fd07d4bc492b22bdee
shafrazi <shafrazi@live.com>
2019-07-18 00:37:43 +0530

    add civicinfo api

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -19,18 +19,23 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
 
+def legislators_by_zipcode(zipcode)
+  legislators = civicinfo.representative_info_by_address(address: zipcode, levels: "country", roles: ["legislatorUpperBody", "legislatorLowerBody"])
+  legislators = legislators.officials
+  legislator_names = []
+  legislators.each do |legislator|
+    legislator_names << legislator.name
+  end
+  legislator_names
+end
+
 
 data = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
 data.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   begin
-    legislators = civicinfo.representative_info_by_address(address: zipcode, levels: "country", roles: ["legislatorUpperBody", "legislatorLowerBody"])
-    legislators = legislators.officials
-    legislator_names = []
-    legislators.each do |legislator|
-      legislator_names << legislator.name
-    end
+    legislators_by_zipcode(zipcode)
   rescue
     "You can find your representative by visiting www.commoncause.org/take-action/find-elected-officials"
   end
